e6687fda4e08dc26e9632a5933e6fcf3ff273320
Rishabh Goswami <rgoswami@csod.com>
2022-01-10 10:38:37 +0530

    Initial commit

---
+++ packages/react-forms/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button } from '@rishabh/reusable-inventory';
+
+function App() {
+	return (
+		<div className='App'>
+			<Button />
+		</div>
+	);
+}
+
+export default App;
