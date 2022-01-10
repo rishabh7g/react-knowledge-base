a065339b1be180b23d7d7c753bf588ed7aa07960
Rishabh Goswami <rgoswami@csod.com>
2022-01-10 10:48:21 +0530

    Test husky

--- packages/react-forms/src/App.tsx
+++ packages/react-forms/src/App.tsx
@@ -2,11 +2,11 @@ import './App.css';
 import { Button } from '@rishabh/reusable-inventory';
 
 function App() {
-	return (
-		<div className='App'>
-			<Button />
-		</div>
-	);
+  return (
+    <div className='App'>
+      <Button />
+    </div>
+  );
 }
 
 export default App;
