408bcbf6b263f18d8d9d030dde208d4eea21a02f
Rishabh Goswami <rgoswami@csod.com>
2022-01-27 15:09:36 +0530

    Use new Fetch component

--- packages/reusable-inventory/src/index.tsx
+++ packages/reusable-inventory/src/index.tsx
@@ -2,4 +2,5 @@ import { Button } from './components/button';
 import * as util from './util';
 import * as types from './types/axios';
 import { useAxios } from './hooks/useAxios';
-export { Button, util, useAxios, types };
+import { Fetch } from './components/fetch';
+export { Button, util, useAxios, types, Fetch };
