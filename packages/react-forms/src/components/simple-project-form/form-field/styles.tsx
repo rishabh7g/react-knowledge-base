c9be4d67e035c669c7b34f1bcd391c84859b1655
Rishabh Goswami <rgoswami@csod.com>
2022-02-01 16:19:00 +0530

    Delete react-form project

--- packages/react-forms/src/components/simple-project-form/form-field/styles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Input = styled.input`
-  height: 1.7em;
-  padding: 0.5em;
-  margin: 0.5em 0;
-  width: 80%;
-  border: 1px solid #aaaaaa;
-`;
