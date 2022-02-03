9844817ba5fae9caeef7704a8c0c6c7fd81b928b
Rishabh Goswami <rgoswami@csod.com>
2022-02-03 19:36:49 +0530

    Update handlers and manifest file

--- packages/react-concepts/src/mocks/handlers.ts
+++ packages/react-concepts/src/mocks/handlers.ts
@@ -7,11 +7,17 @@ const successResponse = {
   completed: false,
 };
 
+const errorResponse = {
+  errorMessage: 'User authentication failed',
+};
 export const handlers = [
+  rest.get('https://jsonplaceholder.typicode.com/todos/1', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.delay(500), ctx.json(successResponse));
+  }),
   rest.get(
-    '/https://jsonplaceholder.typicode.com/todos/1_success',
+    'https://jsonplaceholder.typicode.com/todos/1_error',
     (req, res, ctx) => {
-      return res(ctx.status(200), ctx.delay(500), ctx.json(successResponse));
+      return res(ctx.status(403), ctx.delay(500), ctx.json(errorResponse));
     }
   ),
 ];
