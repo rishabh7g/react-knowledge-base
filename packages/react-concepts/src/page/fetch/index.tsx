7171e9b9f45bbfbc54ea821d8001783ecf8b1c0e
Rishabh Goswami <rgoswami@csod.com>
2022-02-03 19:39:35 +0530

    Update <Fetch /> to accept <Loading />
    Add <Loading /> to <Fetch />

--- packages/react-concepts/src/page/fetch/index.tsx
+++ packages/react-concepts/src/page/fetch/index.tsx
@@ -1,9 +1,10 @@
 import { Fetch } from '@rishabh/reusable-inventory';
 
 const FetchExample = () => {
+  const url: string = 'https://jsonplaceholder.typicode.com/todos/1_error';
   return (
     <div>
-      <Fetch url='https://jsonplaceholder.typicode.com/todos/1'>
+      <Fetch url={url}>
         {({ userId, title }) => (
           <>
             <h1>{title}</h1>
--- packages/reusable-inventory/src/components/fetch/index.tsx
+++ packages/reusable-inventory/src/components/fetch/index.tsx
@@ -1,20 +1,26 @@
 import { ReactNode } from 'react';
 import { useAxios } from '../..';
 import { METHOD_TYPE, STATUS_TYPE } from '../../types/axios';
+import { Loading } from '../loading';
 
 type FetchProps = {
   url: string;
   method?: METHOD_TYPE;
   requestPayload?: any;
   headersPayload?: any;
+  onLoading?: ReactNode | ((...args: any[]) => ReactNode);
+  onError?: ReactNode | ((...args: any[]) => ReactNode);
   children: ReactNode | ((...args: any[]) => ReactNode);
 };
+
 export const Fetch = ({
   url,
   method = METHOD_TYPE.GET,
   requestPayload,
   headersPayload,
   children,
+  onLoading = () => <Loading />,
+  onError = (error: string) => <div>{JSON.stringify(error)}</div>,
 }: FetchProps) => {
   const { error, response, status } = useAxios({
     url,
@@ -22,14 +28,18 @@ export const Fetch = ({
     body: requestPayload,
     headers: headersPayload,
   });
+
+  const loadingElement =
+    typeof onLoading === 'function' ? onLoading() : onLoading;
+
   switch (status) {
     case STATUS_TYPE.IDLE:
       return null;
     case STATUS_TYPE.LOADING:
-      return <div>Loading....</div>;
+      return loadingElement;
     case STATUS_TYPE.SUCCESS:
       return typeof children === 'function' ? children(response) : children;
     case STATUS_TYPE.FAILED:
-      return <div>{error}</div>;
+      return typeof onError === 'function' ? onError(error) : onError;
   }
 };
